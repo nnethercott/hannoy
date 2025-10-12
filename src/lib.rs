69e04a6db0e4d1cb69e4f03d24eef09352ff16d0
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2025-10-12 00:34:56 -0700

    lifetime and roaring doc (#106)
    * lifetime and roaring doc
    
    * Update src/roaring.rs
    
    * lifetime and roaring
    
    * Update src/lib.rs
    
    ---------
    
    Co-authored-by: Nate Nethercott <nathaniel.nethercott@mistral.ai>

--- src/lib.rs
+++ src/lib.rs
@@ -102,6 +102,7 @@ use metadata::{Metadata, MetadataCodec};
 use node::{Node, NodeCodec};
 use node_id::{NodeId, NodeMode};
 pub use reader::{QueryBuilder, Reader};
+pub use roaring::RoaringBitmapCodec;
 pub use writer::{HannoyBuilder, Writer};
 
 /// The set of types used by the [`Distance`] trait.
