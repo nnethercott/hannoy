b1451709143df0d9dc446f3d49850f4510b5d002
Kerollmops <clement@meilisearch.com>
2026-02-25 12:23:35 +0100

    Pull nested rtxns from a pool

--- src/parallel.rs
+++ src/parallel.rs
@@ -5,10 +5,11 @@ use roaring::RoaringBitmap;
 
 use crate::key::Key;
 use crate::node::{Item, Links, Node};
-use crate::node_id::NodeId;
+use crate::node_id::{NodeId, NodeMode};
 use crate::{Database, Distance, Error, ItemId, Result};
 
 pub(crate) struct FrozenReader<'t, D> {
+    rtxns_pool: crossbeam_channel::Receiver<RoTxn<'t, WithoutTls>>,
     rtxns: thread_local::ThreadLocal<RoTxn<'t, WithoutTls>>,
     index: u16,
     database: Database<D>,
@@ -16,8 +17,9 @@ pub(crate) struct FrozenReader<'t, D> {
 
 impl<'t, D: Distance> FrozenReader<'t, D> {
     pub fn new(wtxn: &'t mut RwTxn<'_>, index: u16, database: Database<D>) -> Result<Self> {
-        let num_threads = rayon::current_num_threads();
-        let (sender, receiver) = crossbeam_channel::bounded(num_threads);
+        // We make sure to have one more thread so the current/main thread has a nested rtxn.
+        let num_threads = rayon::current_num_threads() + 1;
+        let (sender, rtxns_pool) = crossbeam_channel::bounded(num_threads);
 
         // Sequentially generate read transactions from the writer transaction
         for _ in 0..num_threads {
@@ -25,27 +27,18 @@ impl<'t, D: Distance> FrozenReader<'t, D> {
             sender.try_send(rtxn).unwrap();
         }
 
-        // To clarify that we are done sending read transactions
-        drop(sender);
-
-        // Store the read transactions in the thread local for later use
-        let rtxns = thread_local::ThreadLocal::new();
-        rayon::broadcast(|_| {
-            let _ = rtxns.get_or(|| receiver.try_recv().unwrap());
-        });
-
-        Ok(Self { rtxns, index, database })
+        Ok(Self { rtxns_pool, rtxns: thread_local::ThreadLocal::new(), index, database })
     }
 
     pub fn item<'a>(&'a self, item_id: ItemId) -> Result<Item<'a, D>> {
-        let rtxn = self.rtxns.get().expect("missing nested read txn from the thread local");
+        let rtxn = self.rtxns.get_or(|| self.rtxns_pool.try_recv().unwrap());
         let key = Key::item(self.index, item_id);
         // key is a `Key::item` so returned result must be a Node::Item
         self.database.get(rtxn, &key)?.and_then(|node| node.item()).ok_or(Error::missing_key(key))
     }
 
     pub fn links<'a>(&'a self, item_id: ItemId, level: usize) -> Result<Links<'a>> {
-        let rtxn = self.rtxns.get().expect("missing nested read txn from the thread local");
+        let rtxn = self.rtxns.get_or(|| self.rtxns_pool.try_recv().unwrap());
         let key = Key::links(self.index, item_id, level as u8);
         // key is a `Key::item` so returned result must be a Node::Item
         self.database.get(rtxn, &key)?.and_then(|node| node.links()).ok_or(Error::missing_key(key))
@@ -57,14 +50,18 @@ impl<'t, D: Distance> FrozenReader<'t, D> {
         layer: u8,
     ) -> heed::Result<impl Iterator<Item = heed::Result<((ItemId, u8), Cow<'_, RoaringBitmap>)>>>
     {
-        let rtxn = self.rtxns.get().expect("missing nested read txn from the thread local");
+        let rtxn = self.rtxns.get_or(|| self.rtxns_pool.try_recv().unwrap());
         Ok(self.database.lazily_decode_data().iter(rtxn)?.filter_map(move |result| {
             let (key, value) = match result {
                 Ok(value) => value,
                 Err(e) => return Some(Err(e)),
             };
 
-            let Key { node: NodeId { item: item_id, layer: level, .. }, .. } = key;
+            let Key { node: NodeId { item: item_id, layer: level, mode: NodeMode::Links }, .. } =
+                key
+            else {
+                return None;
+            };
 
             if level != layer {
                 return None;
@@ -84,14 +81,18 @@ impl<'t, D: Distance> FrozenReader<'t, D> {
         &self,
     ) -> heed::Result<impl Iterator<Item = heed::Result<((ItemId, u8), Cow<'_, RoaringBitmap>)>>>
     {
-        let rtxn = self.rtxns.get().expect("missing nested read txn from the thread local");
+        let rtxn = self.rtxns.get_or(|| self.rtxns_pool.try_recv().unwrap());
         Ok(self.database.lazily_decode_data().iter(rtxn)?.filter_map(move |result| {
             let (key, value) = match result {
                 Ok(value) => value,
                 Err(e) => return Some(Err(e)),
             };
 
-            let Key { node: NodeId { item: item_id, layer: level, .. }, .. } = key;
+            let Key { node: NodeId { item: item_id, layer: level, mode: NodeMode::Links }, .. } =
+                key
+            else {
+                return None;
+            };
 
             match value.decode() {
                 Ok(Node::Links(Links { links })) => Some(Ok(((item_id, level), links))),
